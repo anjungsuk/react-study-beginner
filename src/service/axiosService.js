3570b79f5da00a2005238bfdbf050e0819692be6
jungukang <rusarmata@naver.com>
2024-12-17 17:55:46 +0900

    feat: env 설정 및 axios config create 생성

--- src/service/axiosService.js
+++ src/service/axiosService.js
@@ -1,3 +1,9 @@
 import axios from "axios";
 
 //axios service 공통 추가 필요 2024.12.13
+export const axiosService = () => {
+
+    return axios.create({
+        baseURL: process.env.REACT_APP_BASEURL
+    })
+}
